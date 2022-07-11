daefd0ea3402defb701553a1cbf3c6b946ea501d
Thor Galle <thorgalle@gmail.com>
2022-07-11 20:03:44 +0200

    Demo validator schema VS "generator" schema

--- src/GuestSchema.ts
+++ src/GuestSchema.ts
@@ -1,7 +1,31 @@
-import Ajv from 'ajv';
+import Ajv from 'ajv/dist/2019';
 import { JSONSchemaBridge } from 'uniforms-bridge-json-schema';
 
-const schema = {
+const options = [
+  {
+    label: 'Developer',
+    value: 'developer',
+  },
+  {
+    label: 'Tester',
+    value: 'tester',
+  },
+  {
+    label: 'Product owner',
+    value: 'product-owner',
+  },
+  {
+    label: 'Project manager',
+    value: 'project-manager',
+  },
+  {
+    label: 'Businessman',
+    value: 'businessman',
+  },
+];
+
+// Used to validate submitted response by ajv 
+const validatorSchema = {
   title: 'Guest',
   type: 'object',
   properties: {
@@ -13,37 +37,34 @@ const schema = {
       minimum: 0,
       maximum: 100,
     },
-    profession: {
-      type: 'string',
-      // uniforms: { component: SelectField },
-      options: [
-        {
-          label: 'Developer',
-          value: 'developer',
-        },
-        {
-          label: 'Tester',
-          value: 'tester',
-        },
-        {
-          label: 'Product owner',
-          value: 'product-owner',
-        },
-        {
-          label: 'Project manager',
-          value: 'project-manager',
-        },
-        {
-          label: 'Businessman',
-          value: 'businessman',
-        },
-      ],
-    },
+    profession: 
+        // For testing: intentionally create an error: only the first two options are actually allowed
+        // by the validator in this case (Developer and Tester). The rest generate an error
+        // when submitting.
+        {type: "string", anyOf: options.slice(0, 2).map(o => ({const: o.value}))}
+    ,
   },
   required: ['firstName', 'lastName'],
 };
 
-const ajv = new Ajv({ allErrors: true, useDefaults: true, strict: "log" });
+// Used to generate the form by Uniforms
+const uniformsSchema = {
+  ...validatorSchema,
+  properties: {
+    ...validatorSchema.properties,
+    profession: {
+      // ...validatorSchema.properties.profession,
+      // The Uniforms bridge will try to map the "anyOf" value to a React DOM attribute,
+      // leading to a warning.
+      // So here we leave it open as a generic "string".
+      // Might be related to this: https://uniforms.tools/docs/api-bridges/#note-on-allofanyofoneof
+      type: "string",
+      options,
+    }
+  }
+}
+
+const ajv = new Ajv({ allErrors: true, useDefaults: true, strict: true });
 
 function createValidator(schema: object) {
   const validator = ajv.compile(schema);
@@ -54,6 +75,6 @@ function createValidator(schema: object) {
   };
 }
 
-const schemaValidator = createValidator(schema);
+const schemaValidator = createValidator(validatorSchema);
 
-export const bridge = new JSONSchemaBridge(schema, schemaValidator);
+export const bridge = new JSONSchemaBridge(uniformsSchema, schemaValidator);
