255aca1bae1d4d81b65a7ca10bcc2de940d86f6a
Thor Galle <thorgalle@gmail.com>
2022-07-11 18:00:59 +0200

    Add MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { AutoForm } from 'uniforms-mui';
+import { bridge as schema } from './GuestSchema';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ maxWidth: '700px', margin: 'auto', marginTop: '1rem' }}>
+       <AutoForm schema={schema} onSubmit={console.log} />;
     </div>
   );
 }
